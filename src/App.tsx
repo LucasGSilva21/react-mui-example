dd6beec841cbf56bfc70edc3629301c85c184a9c
Lucas Gabriel <lucasgsilva2102@gmail.com>
2023-11-20 13:16:34 -0300

    chore: config mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { Button } from "@mui/material";
+
 export const App = () => {
-  return <div className="App">Teste</div>;
+  return (
+    <div className="App">
+      <Button variant="contained" color="primary">
+        Teste
+      </Button>
+    </div>
+  );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { ThemeProvider } from "@mui/material";
+import { LightTheme } from "./shared/themes";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <AppRoutes />
-  </BrowserRouter>
+  <ThemeProvider theme={LightTheme}>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  </ThemeProvider>
 );
