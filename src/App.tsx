5a7264bd38a4758c606f5b0391c7be85d81df11f
Lucas Gabriel <lucasgsilva2102@gmail.com>
2023-11-22 22:07:15 -0300

    feat: add side bar component and context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Button } from "@mui/material";
-import { useAppThemeContext } from "./shared/contexts";
+import { useAppThemeContext, useDrawerContext } from "./shared/contexts";
 
 export const App = () => {
+  const { toggleDrawerOpen } = useDrawerContext();
   const { toggleTheme } = useAppThemeContext();
 
   return (
     <div className="App">
+      <Button variant="contained" color="primary" onClick={toggleDrawerOpen}>
+        Menu
+      </Button>
       <Button variant="contained" color="primary" onClick={toggleTheme}>
-        Teste
+        Trocar tema
       </Button>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
-import { AppThemeProvider } from "./shared/contexts";
+import { AppThemeProvider, DrawerProvider } from "./shared/contexts";
+import { SideBar } from "./shared/components";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <AppThemeProvider>
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <DrawerProvider>
+      <BrowserRouter>
+        <SideBar>
+          <AppRoutes />
+        </SideBar>
+      </BrowserRouter>
+    </DrawerProvider>
   </AppThemeProvider>
 );
