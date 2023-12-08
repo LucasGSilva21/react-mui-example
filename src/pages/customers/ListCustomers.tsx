0d008e4524843e446260c104fc2dafc7c86aa5cc
Lucas Gabriel <lucasgsilva2102@gmail.com>
2023-12-08 18:27:23 -0300

    feat: add useDebounce

--- src/pages/customers/ListCustomers.tsx
+++ src/pages/customers/ListCustomers.tsx
@@ -1,14 +1,29 @@
-import { useMemo } from "react";
+import { useMemo, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import { ListToolBar } from "../../shared/components";
 import { BasePage } from "../../shared/layouts";
+import { CustomersService } from "../../shared/services/api/customers/CustomersService";
+import { useDebounce } from "../../shared/hooks";
 
 export const ListCustomers: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce(1000);
 
   const search = useMemo(() => {
     return searchParams.get("search") || "";
   }, [searchParams]);
+
+  useEffect(() => {
+    debounce(() => {
+      CustomersService.getAll(1, search)
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    });
+  }, [debounce, search]);
 
   return (
     <BasePage
