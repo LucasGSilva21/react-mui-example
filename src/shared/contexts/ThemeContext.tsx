fa65d32cb66e4f2cebfdb4740e3a7a512d8b615f
Lucas Gabriel <lucasgsilva2102@gmail.com>
2023-12-18 17:18:23 -0300

    feat: change default theme

--- src/shared/contexts/ThemeContext.tsx
+++ src/shared/contexts/ThemeContext.tsx
@@ -21,7 +21,7 @@ export const useAppThemeContext = () => {
 };
 
 export const AppThemeProvider: React.FC<PropsWithChildren> = ({ children }) => {
-  const [themeName, setThemeName] = useState<"light" | "dark">("light");
+  const [themeName, setThemeName] = useState<"light" | "dark">("dark");
 
   const toggleTheme = useCallback(() => {
     setThemeName((oldThemeName) =>
